1c30827d390990495c903c442200328bd555ceb4
Amit Reddy <96986946+amitredd@users.noreply.github.com>
2023-11-10 19:32:42 +0530

    Update Home.js

--- client/src/Screens/Home.js
+++ client/src/Screens/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
 
   useEffect(() => {
     const fetchAllBlogs = async () => {
-      const res = await axios.get("http://localhost:5000/api/v1/get/allblogs", {
+      const res = await axios.get("https://blog-quv1.onrender.com/get/allblogs", {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
